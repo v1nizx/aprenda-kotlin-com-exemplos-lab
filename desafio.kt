ec2a7bdff80ff9621131819869f927e9c6961e26
v1nizx <144844828+v1nizx@users.noreply.github.com>
2023-09-15 15:13:06 -0300

    Desafio kotlin respondido

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,35 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
-
 enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
 
-class Usuario
+class Usuario(val nome: String)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ConteudoEducacional(val nome: String, val duracao: Int = 60)
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
-
+class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
     val inscritos = mutableListOf<Usuario>()
-    
+
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        inscritos.add(usuario)
+        println("Usuário ${usuario.nome} matriculado na formação $nome")
+    }
+
+    fun listarInscritos() {
+        println("Inscritos na formação $nome:")
+        inscritos.forEach { println("- ${it.nome}") }
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    val conteudo1 = ConteudoEducacional("Introdução à Kotlin")
+    val conteudo2 = ConteudoEducacional("Programação Orientada a Objetos em Kotlin")
+    val conteudo3 = ConteudoEducacional("Kotlin Avançado")
+
+    val formacaoKotlin = Formacao("Formação Kotlin", listOf(conteudo1, conteudo2, conteudo3))
+
+    val usuario1 = Usuario("Alice")
+    val usuario2 = Usuario("Bob")
+
+    formacaoKotlin.matricular(usuario1)
+    formacaoKotlin.matricular(usuario2)
+
+    formacaoKotlin.listarInscritos()
 }
